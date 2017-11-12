405a8a6de3610ef0f8c5b3b4b3b335a0a5e554e1
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2017-11-12 19:31:59 -0300

    added user method to generate JWT

--- app/models/users.js
+++ app/models/users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const crypto =  require('crypto');
+const jwt = require('jsonwebtoken');
+
 const Schema = mongoose.Schema;
 
 
@@ -26,20 +28,36 @@ const userSchema = Schema({
     salt: String
 });
 
-const BYTES_NUMBER = 16;
+const NUMBER_OF_BYTES = 16;
 const ENCODING = 'hex';
 const ITERATIONS = 1000;
 const KEYLEN = 64;
 const DIGEST = 'sha512';
 
+
 userSchema.methods.setPassword = function(password){
-  this.salt = crypto.randomBytes(BYTES_NUMBER).toString(ENCODING);
+  this.salt = crypto.randomBytes(NUMBER_OF_BYTES).toString(ENCODING);
   this.hash = crypto.pbkdf2Sync(password, this.salt, ITERATIONS, KEYLEN, DIGEST).toString(ENCODING);
 };
 
 userSchema.methods.validPassword = function(password) {
-  var hash = crypto.pbkdf2Sync(password, this.salt, ITERATIONS, KEYLEN, DIGEST).toString(ENCODING);
+  const hash = crypto.pbkdf2Sync(password, this.salt, ITERATIONS, KEYLEN, DIGEST).toString(ENCODING);
   return this.hash === hash;
+};
+
+userSchema.method.generateJwt = function() {
+    const NUMBER_OF_DAYS = 7;
+    const THOUSAND_SECONDS = 1000;
+    const SECRET = 'SECRET';
+    const expiry = new Date();
+    expiry.setDate(expiry.getDate() + NUMBER_OF_DAYS);
+
+    return jwt.sign({
+        _id: this._id,
+        name: this.name,
+        email: this.email,
+        exp: parseInt(expiry.getTime() / THOUSAND_SECONDS)
+    }, SECRET);
 };
 
 mongoose.model("User", userSchema);
