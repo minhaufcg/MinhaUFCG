a6998a481c226f0132edba1ed6f637e763f469e3
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2017-11-12 18:41:11 -0300

    created methods to set and validate user password

--- app/models/users.js
+++ app/models/users.js
@@ -1,16 +1,45 @@
 const mongoose = require('mongoose');
+const crypto =  require('crypto');
 const Schema = mongoose.Schema;
 
 
 const userSchema = Schema({
-    name: {type: String, require: true},
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
     role: {
         type: String, 
         enum: ["student", "teacher", "servant", "admin"],
-        require: true
+        required: true
     },
-    registration: {type: String, require: true},
-    password: {type: String, require: true}
+    registration: {
+        type: String,
+        required: true
+    },
+    hash: String,
+    salt: String
 });
+
+const BYTES_NUMBER = 16;
+const ENCODING = 'hex';
+const ITERATIONS = 1000;
+const KEYLEN = 64;
+const DIGEST = 'sha512';
+
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(BYTES_NUMBER).toString(ENCODING);
+  this.hash = crypto.pbkdf2Sync(password, this.salt, ITERATIONS, KEYLEN, DIGEST).toString(ENCODING);
+};
+
+userSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, ITERATIONS, KEYLEN, DIGEST).toString(ENCODING);
+  return this.hash === hash;
+};
 
 mongoose.model("User", userSchema);
