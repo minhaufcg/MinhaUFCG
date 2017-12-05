91376b94bc5fb1fbdd8c14a8aa68a894d8827c66
gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2017-12-04 23:34:17 -0300

    Created table for all user requests

--- public/js/controllers/manage-controller.js
+++ public/js/controllers/manage-controller.js
@@ -1,5 +1,10 @@
 angular.module('mufcg')
-.controller('ManageCtrl', function($scope, $state) {
-    
-
+.controller('ManageCtrl', function($scope, $state, $window, Request, AuthService) {
+    $scope.initTable = function() {
+      Request.getByAuthor(AuthService.getCurrentUser().id).then(function (res) {
+        $window.localStorage.setItem('requests', JSON.stringify(res.data));
+        $scope.requests = res.data;
+      });
+    };
+    $scope.initTable();
 });
